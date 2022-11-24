474fa5fa4bca29f56e0a5c31548746ed2f2a0bb7
vaibhavrajsingh2001 <vaibhavrajsingh2001@gmail.com>
2022-11-24 22:09:29 +0530

    feat: integrate form submission

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from gcloud import search_role
-from payload import form
+from payload import form, request_text
 
 load_dotenv()
 
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
+SLACK_PRIVATE_CHANNEL_ID = os.getenv("SLACK_PRIVATE_CHANNEL_ID")
 
 # Initializes your app with your bot token and socket mode handler
 app = App(token=SLACK_BOT_TOKEN)
@@ -36,10 +37,67 @@ def open_modal(ack, body, client):
     )
 
 
-@app.view("")
-def handle_view_submission_events(ack, body, logger):
+@app.view("perm_form")
+def handle_view_submission_events(ack, body, client):
     ack()
-    print(body)
+    values = body["view"]["state"]["values"]
+    project = values["project_choice"]["project_choice-action"]["selected_option"][
+        "value"
+    ]
+    duration = values["duration_choice"]["duration_select-action"]["selected_option"][
+        "value"
+    ]
+    email = values["email_input"]["email-action"]["value"]
+
+    permDictList = values["permission_choice"]["perm_select-action"]["selected_options"]
+    perms = []
+
+    for perm in permDictList:
+        perms.append(perm["value"])
+
+    request_config = request_text(project, perms, duration, email)
+
+    client.chat_postMessage(
+        channel=SLACK_PRIVATE_CHANNEL_ID,
+        text="IAM permission requested",
+        blocks=request_config,
+    )
+
+
+@app.message("send")
+def send_text(ack, client):
+    ack()
+
+    request_config = request_text(
+        project="development",
+        perms=[
+            "roles/accesscontextmanager.gcpAccessAdmin",
+            "roles/accessapproval.viewer",
+        ],
+        duration="30",
+        email="vaibhav@deepsource.io",
+    )
+
+    print(request_config)
+
+    result = client.chat_postMessage(
+        channel=SLACK_PRIVATE_CHANNEL_ID,
+        text="IAM permission requested",
+        blocks=request_config,
+    )
+
+    print(result)
+
+
+@app.action("request-approved")
+def handle_some_action(ack, body, logger):
+    ack()
+    logger.info(body)
+
+
+@app.action("request-denied")
+def handle_some_action(ack, body, logger):
+    ack()
     logger.info(body)
 
 
