fba3ec499d691095482aa4b6ff96a0e6afc5a0e0
awalvie <agrawal.vishesh.178@gmail.com>
2022-11-24 18:13:47 +0530

    feat: format with black and run create_permission in subprocess

--- gcloud.py
+++ gcloud.py
@@ -1,25 +1,38 @@
 import sys
 import subprocess
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+
 
 def search_role(term=None):
     if term:
-        output = subprocess.run([f"gcloud iam roles list --format=json --filter='title:{term}'"], shell=True)
+        output = subprocess.run(
+            [f"gcloud iam roles list --format=json --filter='title:{term}'"], shell=True
+        )
         return output
     else:
-        output = subprocess.run([f"gcloud iam roles list --format=json --limit=10"], shell=True)
+        output = subprocess.run(
+            [f"gcloud iam roles list --format=json --limit=10"], shell=True
+        )
         return output
+
 
 def create_permission(project, email, roles, duration):
     for role in roles:
-        expiration_timestamp = datetime.now() + timedelta(minutes=duration)
+        expiration_timestamp = (
+            (datetime.now(timezone.utc) + timedelta(minutes=duration))
+            .replace(microsecond=0)
+            .isoformat()
+        )
+        print(expiration_timestamp)
 
-        base_command = "gcloud projects add-iam-policy-binding "
+        base_command = f"gcloud projects add-iam-policy-binding {project} "
         member = f"--member 'user:{email}' "
         role = f"--role {role} "
-        condition = f"--condition='expression=request.time < timestamp('{expiration_timestamp.isoformat()}'),title=temporary_permission' "
+        condition = f"--condition=\"expression=request.time < timestamp('{expiration_timestamp}'),title=temporary_permission\" "
 
         command = base_command + member + role + condition
         print(command)
 
+        output = subprocess.run([f"{command}"], shell=True)
+        return output
 
